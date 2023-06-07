4571e3766de52adb348ce128647fedb1dcb3471f
Alejandro Junqui <ajunquit@gmail.com>
2023-06-06 21:14:35 -0500

    Update act3pipeline.groovy

--- act3pipeline.groovy
+++ act3pipeline.groovy
@@ -23,12 +23,12 @@ pipeline {
                 archiveArtifacts artifacts: 'results/*.xml'
             }
         }
-        //stage('API tests') {
-        //    steps {
-         //       sh 'make test-api'
-         //       archiveArtifacts artifacts: 'results/*.xml'
-        //    }
-        //}
+        stage('API tests') {
+            steps {
+                sh 'make test-api'
+                archiveArtifacts artifacts: 'results/*.xml'
+            }
+        }
         //stage('e2e tests') {
             //steps {
                 //sh 'make test-e2e'
